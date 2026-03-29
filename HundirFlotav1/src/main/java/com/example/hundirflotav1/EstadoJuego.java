22b7383cd4186161f1e993a730200a78f1fd81e9
Abel Rovira Alvarez <165647659+abel-rovira@users.noreply.github.com>
2026-03-30 01:17:30 +0200

    Update EstadoJuego.java

--- HundirFlotav1/src/main/java/com/example/hundirflotav1/EstadoJuego.java
+++ HundirFlotav1/src/main/java/com/example/hundirflotav1/EstadoJuego.java
@@ -37,38 +37,139 @@ public class EstadoJuego extends Writable implements Serializable {
         this.colocandoBarcos = true;
     }
 
-    public char[][] getTableroJugador() { return tableroJugador; }
-    public void setTableroJugador(char[][] tableroJugador) { this.tableroJugador = tableroJugador; }
-    public char[][] getTableroPC() { return tableroPC; }
-    public void setTableroPC(char[][] tableroPC) { this.tableroPC = tableroPC; }
-    public char[][] getTableroDisparosJugador() { return tableroDisparosJugador; }
-    public void setTableroDisparosJugador(char[][] tableroDisparosJugador) { this.tableroDisparosJugador = tableroDisparosJugador; }
-    public char[][] getTableroDisparosPC() { return tableroDisparosPC; }
-    public void setTableroDisparosPC(char[][] tableroDisparosPC) { this.tableroDisparosPC = tableroDisparosPC; }
-    public int getImpactosJugador() { return impactosJugador; }
-    public void setImpactosJugador(int impactosJugador) { this.impactosJugador = impactosJugador; }
-    public int getImpactosPC() { return impactosPC; }
-    public void setImpactosPC(int impactosPC) { this.impactosPC = impactosPC; }
-    public int getTotalCeldas() { return totalCeldas; }
-    public void setTotalCeldas(int totalCeldas) { this.totalCeldas = totalCeldas; }
-    public List<String> getHistorialDisparos() { return historialDisparos; }
-    public void setHistorialDisparos(List<String> historialDisparos) { this.historialDisparos = historialDisparos; }
-    public List<String> getHistorialDisparosCPU() { return historialDisparosCPU; }
-    public void setHistorialDisparosCPU(List<String> historialDisparosCPU) { this.historialDisparosCPU = historialDisparosCPU; }
-    public boolean isJuegoTerminado() { return juegoTerminado; }
-    public void setJuegoTerminado(boolean juegoTerminado) { this.juegoTerminado = juegoTerminado; }
-    public String getGanador() { return ganador; }
-    public void setGanador(String ganador) { this.ganador = ganador; }
-    public int[] getBarcos() { return barcos; }
-    public void setBarcos(int[] barcos) { this.barcos = barcos; }
-    public boolean[][] getCeldasOcupadas() { return celdasOcupadas; }
-    public void setCeldasOcupadas(boolean[][] celdasOcupadas) { this.celdasOcupadas = celdasOcupadas; }
-    public List<int[]> getUltimoDisparoJugador() { return ultimoDisparoJugador; }
-    public void setUltimoDisparoJugador(List<int[]> ultimoDisparoJugador) { this.ultimoDisparoJugador = ultimoDisparoJugador; }
-    public List<int[]> getUltimoDisparoCPU() { return ultimoDisparoCPU; }
-    public void setUltimoDisparoCPU(List<int[]> ultimoDisparoCPU) { this.ultimoDisparoCPU = ultimoDisparoCPU; }
-    public int getBarcoActualIndex() { return barcoActualIndex; }
-    public void setBarcoActualIndex(int barcoActualIndex) { this.barcoActualIndex = barcoActualIndex; }
-    public boolean isColocandoBarcos() { return colocandoBarcos; }
-    public void setColocandoBarcos(boolean colocandoBarcos) { this.colocandoBarcos = colocandoBarcos; }
+    public char[][] getTableroJugador() {
+        return tableroJugador;
+    }
+
+    public void setTableroJugador(char[][] tableroJugador) {
+        this.tableroJugador = tableroJugador;
+    }
+
+    public char[][] getTableroPC() {
+        return tableroPC;
+    }
+
+    public void setTableroPC(char[][] tableroPC) {
+        this.tableroPC = tableroPC;
+    }
+
+    public char[][] getTableroDisparosJugador() {
+        return tableroDisparosJugador;
+    }
+
+    public void setTableroDisparosJugador(char[][] tableroDisparosJugador) {
+        this.tableroDisparosJugador = tableroDisparosJugador;
+    }
+
+    public char[][] getTableroDisparosPC() {
+        return tableroDisparosPC;
+    }
+
+    public void setTableroDisparosPC(char[][] tableroDisparosPC) {
+        this.tableroDisparosPC = tableroDisparosPC;
+    }
+
+    public int getImpactosJugador() {
+        return impactosJugador;
+    }
+
+    public void setImpactosJugador(int impactosJugador) {
+        this.impactosJugador = impactosJugador;
+    }
+
+    public int getImpactosPC() {
+        return impactosPC;
+    }
+
+    public void setImpactosPC(int impactosPC) {
+        this.impactosPC = impactosPC;
+    }
+
+    public int getTotalCeldas() {
+        return totalCeldas;
+    }
+
+    public void setTotalCeldas(int totalCeldas) {
+        this.totalCeldas = totalCeldas;
+    }
+
+    public List<String> getHistorialDisparos() {
+        return historialDisparos;
+    }
+
+    public void setHistorialDisparos(List<String> historialDisparos) {
+        this.historialDisparos = historialDisparos;
+    }
+
+    public List<String> getHistorialDisparosCPU() {
+        return historialDisparosCPU;
+    }
+
+    public void setHistorialDisparosCPU(List<String> historialDisparosCPU) {
+        this.historialDisparosCPU = historialDisparosCPU;
+    }
+
+    public boolean isJuegoTerminado() {
+        return juegoTerminado;
+    }
+
+    public void setJuegoTerminado(boolean juegoTerminado) {
+        this.juegoTerminado = juegoTerminado;
+    }
+
+    public String getGanador() {
+        return ganador;
+    }
+
+    public void setGanador(String ganador) {
+        this.ganador = ganador;
+    }
+
+    public int[] getBarcos() {
+        return barcos;
+    }
+
+    public void setBarcos(int[] barcos) {
+        this.barcos = barcos;
+    }
+
+    public boolean[][] getCeldasOcupadas() {
+        return celdasOcupadas;
+    }
+
+    public void setCeldasOcupadas(boolean[][] celdasOcupadas) {
+        this.celdasOcupadas = celdasOcupadas;
+    }
+
+    public List<int[]> getUltimoDisparoJugador() {
+        return ultimoDisparoJugador;
+    }
+
+    public void setUltimoDisparoJugador(List<int[]> ultimoDisparoJugador) {
+        this.ultimoDisparoJugador = ultimoDisparoJugador;
+    }
+
+    public List<int[]> getUltimoDisparoCPU() {
+        return ultimoDisparoCPU;
+    }
+
+    public void setUltimoDisparoCPU(List<int[]> ultimoDisparoCPU) {
+        this.ultimoDisparoCPU = ultimoDisparoCPU;
+    }
+
+    public int getBarcoActualIndex() {
+        return barcoActualIndex;
+    }
+
+    public void setBarcoActualIndex(int barcoActualIndex) {
+        this.barcoActualIndex = barcoActualIndex;
+    }
+
+    public boolean isColocandoBarcos() {
+        return colocandoBarcos;
+    }
+
+    public void setColocandoBarcos(boolean colocandoBarcos) {
+        this.colocandoBarcos = colocandoBarcos;
+    }
 }
